a829c20d9eaf5080581485ca278b069053f762f7
Angelo <125922847+Angelb2b@users.noreply.github.com>
2023-09-21 22:15:00 +0200

    Update ProtectedRoutes.js

--- src/middlewares/ProtectedRoutes.js
+++ src/middlewares/ProtectedRoutes.js
@@ -1,43 +1,36 @@
-import React, { useEffect, useState } from "react";
-import jwtDecode from 'jwt-decode'
+import React, { useEffect } from "react";
+import jwtDecode from 'jwt-decode';
 import Login from "../Components/Pages/Login";
-import {Outlet, useLocation, useNavigate} from 'react-router-dom'
-import { useSelector } from "react-redux";
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
-const auth = () => {
-    return JSON.parse(localStorage.getItem("userLoggedIn"))
+const getAuth = () => {
+  return JSON.parse(localStorage.getItem("userLoggedIn"));
 };
 
 export const useSession = () => {
-    const session = auth();
-    const decodedSession = session ? jwtDecode(session) : null;
-    const location = useLocation()
+  const session = getAuth();
+  const decodedSession = session ? jwtDecode(session) : null;
+  const location = useLocation();
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    if (!session) {
+      if (location.pathname === '/login' || location.pathname === '/') {
+        return; // Prevent further navigation attempts
+      }
+      
+      navigate('/login', { replace: true });
+    }
+  }, [session, location.pathname, navigate]);
 
-    const navigate = useNavigate()
-
-    useEffect(() => {
-
-
-        if(!localStorage.getItem("userLoggedIn")) {
-            
-            if (location.pathname === '/') navigate('/')
-            else navigate('/login', { replace: true })
-        } 
-    }, [navigate, session])
-    
-
-    return decodedSession;
-}
-
+  return decodedSession;
+};
 
 const ProtectedRoutes = () => {
+  const isAuthorized = getAuth();
+  const session = useSession();
 
-    const navigate = useNavigate()
-    const isAuthorized = auth();
-    const session = useSession();
-    
-    return isAuthorized ? <Outlet /> : <Login />
-}
+  return isAuthorized ? <Outlet /> : <Login />;
+};
 
 export default ProtectedRoutes;
