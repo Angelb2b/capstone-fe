28c5dafd5cdd40da7799b63ffff92fc3421b24db
Angelo <125922847+Angelb2b@users.noreply.github.com>
2023-09-27 16:17:14 +0200

    Update commentsSlice.js

--- src/Store/commentsSlice.js
+++ src/Store/commentsSlice.js
@@ -1,116 +1,65 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk} from "@reduxjs/toolkit";
 
-// Azione asincrona per ottenere i commenti di un post
-export const getCommentsByPostId = createAsyncThunk(
-  "comments/getCommentsByPostId",
-  async (postId) => {
-    try {
-      const response = await fetch(
-        `${process.env.REACT_APP_SERVER_BASE_URL}/blogPosts/${postId}/comments`
-      );
-      if (!response.ok) {
-        throw new Error("Failed to fetch comments");
-      }
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      throw new Error(`Error fetching comments: ${error.message}`);
-    }
-  }
-);
 
-// Azione asincrona per creare un nuovo commento
-export const postNewComment = createAsyncThunk(
-  "comments/postNewComment",
-  async ({ postId, payload }) => {
-    try {
-      const response = await fetch(
-        `${process.env.REACT_APP_SERVER_BASE_URL}/blogPosts/${postId}/newComment`,
-        {
-          method: "POST",
-          body: JSON.stringify(payload),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Failed to create comment");
-      }
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      throw new Error(`Error creating comment: ${error.message}`);
-    }
-  }
-);
-
-// Azione asincrona per eliminare un commento
-export const deleteComment = createAsyncThunk(
-  "comments/deleteComment",
-  async ({ postId, commentId }) => {
-    try {
-      const response = await fetch(
-        `${process.env.REACT_APP_SERVER_BASE_URL}/blogPosts/${postId}/deleteComment/${commentId}`,
-        {
-          method: "DELETE",
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Failed to delete comment");
-      }
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      throw new Error(`Error deleting comment: ${error.message}`);
-    }
-  }
-);
 
 const initialState = {
-  comments: [],
-  status: "idle",
-  error: null,
-};
+    comments: [],
+    status: 'idle'
+}
 
 const commentsSlice = createSlice({
-  name: "comments",
-  initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(getCommentsByPostId.pending, (state) => {
-        state.status = "loading";
-      })
-      .addCase(getCommentsByPostId.fulfilled, (state, action) => {
-        state.status = "succeeded";
-        state.comments = action.payload;
-      })
-      .addCase(getCommentsByPostId.rejected, (state, action) => {
-        state.status = "failed";
-        state.error = action.error.message;
-      })
-      .addCase(postNewComment.pending, (state) => {
-        state.status = "loading";
-      })
-      .addCase(postNewComment.fulfilled, (state) => {
-        state.status = "succeeded";
-      })
-      .addCase(postNewComment.rejected, (state, action) => {
-        state.status = "failed";
-        state.error = action.error.message;
-      })
-      .addCase(deleteComment.pending, (state) => {
-        state.status = "loading";
-      })
-      .addCase(deleteComment.fulfilled, (state) => {
-        state.status = "succeeded";
-      })
-      .addCase(deleteComment.rejected, (state, action) => {
-        state.status = "failed";
-        state.error = action.error.message;
-      });
-  },
-});
+    name: 'comment',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+        .addCase(getCommentsByPostId.fulfilled, (state, action) => {
+            state.comments = action.payload
+        })
 
-export default commentsSlice.reducer;
+        .addCase(postNewComment.fulfilled, (state, action) => {
+            state.status = 'idle'
+        })
+
+        .addCase(deleteComment.fulfilled, (state, action) => {
+            state.status = 'idle'
+        })
+    }
+})
+
+export default commentsSlice.reducer
+
+export const getCommentById = createAsyncThunk('commentById/get', async (ids) => {
+    console.log(ids)
+    const res = await fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/blogPosts/${ids.post}/comments/${ids.comment}`, {})
+    const data = await res.json()
+    return data
+})
+
+export const getCommentsByPostId = createAsyncThunk('commentsByPostId/get', async (id) => {
+    const res = await fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/blogPosts/${id}/comments`, {})
+    const data = await res.json()
+    return data
+})
+
+export const postNewComment = createAsyncThunk('comment/post', async(datas) => {
+    console.log(datas)
+    const postRes = await fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/blogPosts/${datas.id.id}/newComment`, {
+        method: "POST",
+        body: JSON.stringify(datas.payload),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+    const res = await postRes.json()
+    return res
+})
+
+export const deleteComment = createAsyncThunk('comment/delete', async(comment) => {
+    console.log(comment)
+    const del = await fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/blogPosts/${comment.postId}/deleteComment/${comment.comment._id}`, {
+        method: "DELETE"
+    });
+    const res = await del.json()
+    return res
+})
